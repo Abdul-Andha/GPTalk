8304e44d207620fdca2bd80de270b34a91d43ae6
Dameme1 <tameemisone786@gmail.com>
2023-04-23 18:51:50 -0400

    Resolving Issues

--- GPTalk/MainChatNav/ChatNavViewController.swift
+++ GPTalk/MainChatNav/ChatNavViewController.swift
@@ -12,11 +12,11 @@ import StreamChatUI
 
 class MainChatNav: ChatChannelListVC {
     override open func setUpAppearance() {
-            super.setUpAppearance()
-            title = "Chats"
+        super.setUpAppearance()
+        title = "Chats"
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "plus.app.fill"), style: .plain, target: self, action: #selector(addChat))
-
+        
         // Create the logout button
         let logoutButton = UIButton(type: .system)
         logoutButton.setTitle("Logout", for: .normal)
@@ -38,22 +38,22 @@ class MainChatNav: ChatChannelListVC {
             textField.placeholder = "Username(s)"
         }
         let createAction = UIAlertAction(title: "Create", style: .default) { _ in
-
+            
             guard let usernames = alertController.textFields?.first?.text else {return}
-
+            
             let participantIds = usernames.components(separatedBy: ",").map { $0.trimmingCharacters(in: .whitespacesAndNewlines) }
             
             // TODO: Create a new group chat with the specified participants
             print("Creating group chat with participants \(participantIds)")
             self.createChat(with: participantIds)
-
+            
             guard let username = alertController.textFields?.first?.text else {
                 return
             }
             // TODO: Create a new chat with the specified user
             print("Creating chat with user \(username)")
-                
-
+            
+            
         }
         let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
         alertController.addAction(createAction)
@@ -61,79 +61,67 @@ class MainChatNav: ChatChannelListVC {
         present(alertController, animated: true)
     }
     
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-    // functionality to create new chats. Popup whre the user can enter a name of another user to create a chat with them
-    
-    @objc func addTapped() {
-        let alert = UIAlertController(title: "Create new chat", message: "Enter the username of the user you want to chat with", preferredStyle: .alert)
-        alert.addTextField(configurationHandler: nil)
-=======
-    private func createChat(with usernames: [String]) {
-        printAllUsers()
-=======
-    private func createChat(with usernames: [String]) {
->>>>>>> main
-        // Use the `ChatClient` to create a `ChatChannelController` with a list of user ids
-        let channelController = try? ChatClient.shared.channelController(
-            createDirectMessageChannelWith: Set(usernames),
-            isCurrentUserMember: true,
-            name: nil,
-            imageURL: nil,
-            extraData: ["test": "test"]
-        )
-<<<<<<< HEAD
->>>>>>> Stashed changes
-=======
->>>>>>> main
-
-        // Call `ChatChannelController.synchronize` to create the channel.
-        channelController?.synchronize { error in
-            if let error = error {
-                print("Error creating channel: \(error)")
-            } else {
-                print("Channel created successfully.")
+        private func createChat(with usernames: [String]) {
+            printAllUsers()
+            
+            // Use the `ChatClient` to create a `ChatChannelController` with a list of user ids
+            let channelController = try? ChatClient.shared.channelController(
+                createDirectMessageChannelWith: Set(usernames),
+                isCurrentUserMember: true,
+                name: nil,
+                imageURL: nil,
+                extraData: ["test": "test"]
+            )
+            
+            
+            // Call `ChatChannelController.synchronize` to create the channel.
+            channelController?.synchronize { error in
+                if let error = error {
+                    print("Error creating channel: \(error)")
+                } else {
+                    print("Channel created successfully.")
+                }
             }
         }
-    }
-    
-    //call to print all users. useful during development. delete after.
-    func printAllUsers() {
-        let controller = ChatClient.shared.userListController(
-            query: .init()
-        )
-
-        controller.synchronize { error in
-            if let error = error {
-                // handle error
-                print("err")
-                print(error)
-            } else {
-                // access users
-                print("succ")
-                print(controller.users)
-                print(controller.users.forEach { user in
-                    print(user.id)
-                })
+        
+        //call to print all users. useful during development. delete after.
+        func printAllUsers() {
+            let controller = ChatClient.shared.userListController(
+                query: .init()
+            )
+            
+            controller.synchronize { error in
+                if let error = error {
+                    // handle error
+                    print("err")
+                    print(error)
+                } else {
+                    // access users
+                    print("succ")
+                    print(controller.users)
+                    print(controller.users.forEach { user in
+                        print(user.id)
+                    })
+                }
             }
         }
-    }
-    
-    
-    
-    //logout button code starts here
-    @objc func onLogOutTapped(_ sender: Any) {
-        showConfirmLogoutAlert()
+        
+        
+        
+        //logout button code starts here
+        @objc func onLogOutTapped(_ sender: Any) {
+            showConfirmLogoutAlert()
+        }
+        
+        private func showConfirmLogoutAlert() {
+            let alertController = UIAlertController(title: "Log out of your account?", message: nil, preferredStyle: .alert)
+            let logOutAction = UIAlertAction(title: "Log out", style: .destructive) { _ in
+                NotificationCenter.default.post(name: Notification.Name("logout"), object: nil)
+            }
+            let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+            alertController.addAction(logOutAction)
+            alertController.addAction(cancelAction)
+            present(alertController, animated: true)
+        }
     }
 
-    private func showConfirmLogoutAlert() {
-        let alertController = UIAlertController(title: "Log out of your account?", message: nil, preferredStyle: .alert)
-        let logOutAction = UIAlertAction(title: "Log out", style: .destructive) { _ in
-            NotificationCenter.default.post(name: Notification.Name("logout"), object: nil)
-        }
-        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
-        alertController.addAction(logOutAction)
-        alertController.addAction(cancelAction)
-        present(alertController, animated: true)
-    }
-}
